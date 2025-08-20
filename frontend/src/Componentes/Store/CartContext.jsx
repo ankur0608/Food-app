faec721bb8a78a91cdf40b4500aa40a253f896d4
Ankur Patel <ankurp0608@gmail.com>
2025-08-20 13:09:22 +0530

    Update CartContext.jsx

--- frontend/src/Componentes/Store/CartContext.jsx
+++ frontend/src/Componentes/Store/CartContext.jsx
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 const CartContext = createContext({
